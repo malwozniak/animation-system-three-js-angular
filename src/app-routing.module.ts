6c64d1d4980e63473f10937dcbe4324aaac69de6
malwozniak <mgrztwnk5@gmail.com>
2022-08-09 14:46:15 +0200

    1 file modified, 1 added

---
+++ src/app-routing.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SystemComponent } from './home';
+
+const routes: Routes = [
+  { path: '', component: SystemComponent  
+  }
+];
